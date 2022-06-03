b4125ef88d61ea784bae1d8dbdd646c178b54b39
Shubhranshu Github <1248830+shubhranshu@users.noreply.github.com>
2022-06-03 15:07:27 +1200

    feat: initialised app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import DefaultLayout from '../components/layouts/_defaultLayout';
+import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <DefaultLayout>
+      <Component {...pageProps} />
+    </DefaultLayout>
+  );
 }
 
-export default MyApp
+export default MyApp;
