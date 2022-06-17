6c4146fe0309f7ff097d138cec014e4a0775d9fe
Shubhranshu Github <1248830+shubhranshu@users.noreply.github.com>
2022-06-17 16:39:24 +1200

    reference-baseapp

--- refApp/src/components/upload/index.ts
+++
@@ -1,9 +0,0 @@
-export * from './type';
-
-export { default as BlockContent } from './BlockContent';
-export { default as RejectionFiles } from './RejectionFiles';
-export { default as MultiFilePreview } from './MultiFilePreview';
-
-export { default as UploadAvatar } from './UploadAvatar';
-export { default as UploadMultiFile } from './UploadMultiFile';
-export { default as UploadSingleFile } from './UploadSingleFile';
